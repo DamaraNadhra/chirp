9ce8b23a69c80abe794b58f23832545b177447da
Damara Nadhra Matin <dmat0262@uni.sydney.edu.au>
2024-12-28 18:29:10 +1100

    Next image tag, style up

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,42 @@
+import { clerkClient, User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id, 
+    username: user.username, 
+    imageUrl: user.imageUrl
+  };
+};
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.db.post.findMany();
+    const posts =  await ctx.db.post.findMany({ 
+      take: 100,
+    });
+
+    const users = (await (await clerkClient()).users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100
+    })).data.map(filterUserForClient);
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author || !author.username) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR", 
+        message: "author not found"
+      })
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username
+        }
+      }
+    })
   })
 });
